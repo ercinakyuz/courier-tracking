64ca93667bbb25277b53c2c7e15ff03fcaf193f1
ercinakyuz <b.ercin.akyuz@gmail.com>
2022-05-30 19:41:01 +0300

    swagger dependency added

--- src/test/java/com/migros/couriertracking/api/integration/CourierIntegrationTest.java
+++ src/test/java/com/migros/couriertracking/api/integration/CourierIntegrationTest.java
@@ -24,7 +24,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.ResultActions;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
